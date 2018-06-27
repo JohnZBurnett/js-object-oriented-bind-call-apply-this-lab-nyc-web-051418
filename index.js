6e5bbc9f5e2ff599e843fec78bdbbd7a47b5b6f4
JohnZBurnett <tuninator@gmail.com>
2018-06-27 17:40:51 -0400

    finish

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(cb) {
+  return cb();
+}
+
+function setThisWithCall(cb, thisValue, arg) {
+  return cb.call(thisValue, arg);
+}
+
+function setThisWithApply(cb, thisValue, args) {
+  return cb.apply(thisValue, args);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue); 
+}
